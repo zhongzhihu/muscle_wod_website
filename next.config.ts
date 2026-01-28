dd7c3f60a91a6916eca860c0f0ba0ce32a6d3284
Zhongzhi Hu <zhongzhihu@hotmail.com>
2026-01-28 20:21:47 +0100

    Configure Next.js for GitHub Actions deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? "/muscle_wod_website" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
