5ef6767022736132e0937316eb6b900e85cda3a1
Zhongzhi Hu <zhongzhihu@hotmail.com>
2026-01-28 21:51:26 +0100

    fix for netlify deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/muscle_wod_website" : "",
   images: {
     unoptimized: true,
   },
